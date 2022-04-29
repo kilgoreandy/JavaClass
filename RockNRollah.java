35cf8e3288ab3e5ae690eb633e3936a75b1432b9
kilgoreandy <92450298+kilgoreandy@users.noreply.github.com>
2022-04-29 14:47:55 -0500

    Add files via upload

---
+++ RockNRollah.java
@@ -0,0 +1,31 @@
+/*
+ * RockNRollah.java
+ */
+
+import java.util.Random;
+
+/**
+ *
+ * @author StudentName
+ */
+public class RockNRollah {
+       
+    public static void main(String[] args) {
+    	// Create a Random object
+    	// Initialize a couple of variables to hold the values of the two dice
+    	// Initialize the array that will hold the results
+    	/* Hypothetical questions: (no, I don't want answers)
+    	 *   How many different sum values do we need to account for?
+    	 *   How big does the array need to be?
+    	 *   Why might you make it (slightly) larger? (Note: my answer does not use an unnecessarily large array, and I'm not encouraging you to; just giving you something to think about) 
+    	 */
+    	// Roll the dice (a lot) and sum and tally the results
+        // Display the frequencies
+        System.out.println("Results after " + rolls + " rolls");
+        System.out.println("-------------------------");
+        System.out.println("Value\tFrequency");
+        // Loop, displaying the rows for the body of the table, each on its own line.
+        //   The required format for each row is to display the sum, followed by a tab, followed by the number of times that sum came up. Do not include any additional formatting
+    }
+    
+}
