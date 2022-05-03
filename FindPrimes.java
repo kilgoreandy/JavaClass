8ee7ecd9c8f5c32aaaa48d03e14d1458e17ac35b
kilgoreandy <92450298+kilgoreandy@users.noreply.github.com>
2022-05-03 09:53:33 -0500

    added

--- FindPrimes.java
+++ FindPrimes.java
@@ -7,8 +7,8 @@
  * @author Andy Kilgore
  */
 public class FindPrimes {
-    //We dont care about multipying 1 because regardless that does not change anything so we start at 2. 
-    static int count = 2;
+    //Started at 0 just for giggles. 
+    static int count = 0;
     public static void main(String[] args) {
         System.out.println("Prime numbers < 10k:");
         //while loop counts  until 10001
@@ -27,21 +27,20 @@ public class FindPrimes {
     }
      
     public static boolean isPrime(int num) {
-        //We dont care about multipying 1 because regardless that does not change anything so we start at 2.
-    int i = 2;
-    boolean prime = true;
-    while (i <= java.lang.Math.sqrt(num)) {
-      // condition for nonprime number
-      if (num % i == 0) {
-          //if it can be divisible by i then it is not prime. Set prime to false if it can be. 
-        prime = false;
-        break;
-      }
-    //increment i counter
-      ++i;
+// We do not really care about any num that is less than 2 so we hardcode to return prime false. 
+        boolean prime = false;
+    	if(num < 2) 
+            return prime;
+        
+       //starting with 2 we determine if its divisible evenly if not then its def prime. 
+        for(int i=2; i <= Math.sqrt(num); i++) {
+        	
+            if(num % i == 0)
+                return prime;
+        }
+        
+        // Anything else has to be prime so we return prime = true back to the main function. 
+        prime = true;
+        return prime;
     }
-    //return to callin method result of isPrime
-    return prime;
-     
-  }
 }
