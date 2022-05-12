56c5e142f7dc878ebd232a566548a45ec78baf9a
kilgoreandy <92450298+kilgoreandy@users.noreply.github.com>
2022-05-12 11:07:04 -0500

    i think its fixed

--- IntegerSet.java
+++ IntegerSet.java
@@ -6,55 +6,63 @@
  *
  * @author Andy Kilgore
  */
-
-
-/* 
- * Homework Prompt
- * Create class IntegerSet. Each IntegerSet
- * object can hold integers in the range 0–100. The set is represented by an
- * array of booleans. Array element a[i] is true if integer i is in the set.
- * Array element a[j] is false if integer j is not in the set. The noargument constructor initializes the array to the “empty set” (i.e., all
- * false values).
- */
 public class IntegerSet {
-    
+    boolean[] bset; 
     /** 
      * Creates a new instance of IntegerSet   
      */
     // TODO: implement the constructor
-        public IntegerSet(){
-            set boolean[] = {};
-        }
+    public IntegerSet(){
+        bset = new boolean[100];
+    }
     
     /**
      * Return a new IntegerSet containing the union of the two IntegerSet objects
      * passed as arguments
      */
     // TODO: implement the union method
-        public IntegerSet union (IntegerSet i, IntegerSet j){
-            IntegerSet = i + j;
+    public static IntegerSet union (IntegerSet a, IntegerSet b){
+        IntegerSet unionset = new IntegerSet();
+        for(int i=0, j=0; i< 100; i++, j++){
+	        if(a.bset[i]== true || b.bset[j]== true )
+		        unionset.bset[i]=true;
         }
-    
+        return unionset;
+    }
     /**
      * Return a new IntegerSet containing the intersection of the two IntegerSet objects
      * passed as arguments
      */
     // TODO: implement the intersection method
-    
+     public static IntegerSet intersection(IntegerSet a, IntegerSet b){ // sets arr ele to true if both objects have true 
+        IntegerSet interset= new IntegerSet();		
+	    for(int i=0, j=0; i<100; i++, j++){
+	        if(a.bset[i]== true && b.bset[j]== true )
+		    interset.bset[i]=true; 
+	}
+	return interset; 
+    }
+
     /**
      * Inserts an element into the IntegerSet by setting the corresponding
      * value within the set array to true. Returns false if the value was out
      * of range and true otherwise.
      */
     // TODO: implement the insertElement method
-    
+    public IntegerSet insertElement(int data){
+  	    bset[data]= true;  // changes array element to true with the integer given in parameter 
+        return this; 
+    }
     /**
      * Deletes an element from the IntegerSet by setting the corresponding
      * value within the set array to false. Returns false if the value was out
      * of range and true otherwise.
      */
     // TODO: implement the deleteElement method 
-    
+    public IntegerSet deleteElement(int data){
+  	    bset[data]= false;  // changes array element to true with the integer given in parameter 
+        return this; 
+    }
     /**
      * @Override the toString method in the Object class
      * Displays the integers contained by the IntegerSet separated by spaces. 
@@ -64,10 +72,38 @@ public class IntegerSet {
      * { 5 10 }
      */
     // TODO: implement the toString method   
-    
-    /**
-     * Returns true iff the current IntegerSet contains the same integers as
-     * the IntegerSet supplied as an argument
-     */
-	// TODO: implement the isEqualTo method         
+    public String toString(){
+	String string=""; 
+	
+	boolean flag= true; // flag will tell is string has to be "---"
+
+	int k;
+
+	for(int i=0; i<100; ++i){
+	    if(bset[i]==true){
+		flag= false;
+		string=string.concat(String.valueOf(i)); // concat has int parameter and returns
+		string=string.concat(" ");               // a new string. Old string  + int paramter
+	    }
+	 }
+	
+	 if(flag)
+	     string="---";  // string is empty 
+
+	  return string;
+    }
+    public boolean isEqualTo(IntegerSet iSet){  // search both arrays and returns false as soon as one 
+	// of the elements in either objects array is false 
+	boolean flag= true;
+	
+	for(int i=0, j=0; i<100; i++, j++){
+	    if(bset[i]==true  && iSet.bset[j]== true )
+		flag=true;
+	    else
+		flag=false;
+	    
+	}
+	return flag;
+  
+    }  
 }
