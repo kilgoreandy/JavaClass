ee42c5d18e8cae5fc56f3d0dd928bf24b23ffb96
kilgoreandy <92450298+kilgoreandy@users.noreply.github.com>
2022-05-16 15:57:26 -0500

    updated the constructors

--- BasePlusCommissionEmployeeComposition.java
+++ BasePlusCommissionEmployeeComposition.java
@@ -15,8 +15,7 @@ the same functionality.
 */ 
 
 public class BasePlusCommissionEmployeeComposition {
-    BasePlusCommissionEmployee bpce = new BasePlusCommissionEmployee();
-    
+    IllegalArgumentException IAE = new IllegalArgumentException();
     private double baseSalary; // base salary per week
     // Demonstrate composition of (as opposed to inheritance from) a CommissionEmployee object
     
@@ -29,14 +28,21 @@ public class BasePlusCommissionEmployeeComposition {
         this.ssn = ssn;
         this.sales = sales;
         this.rate = rate;
-        this.salary = salary; 
+        this.salary = salary;
+        baseSalary = salary; 
+        CommissionEmployee ce = new CommissionEmployee(first, last, ssn, gross, commission);
     }
     
     // Implement accessors and mutators for all six attributes (stubs appear below)
     // As per the text (demonstrated in its BasePlusCommissionEmployee implementation), throw an IllegalArgumentException if the salary argument is negative
     public void setBaseSalary( double salary ) {
     	// implemented this method
+        if (salary < 0){
+            throw IAE;
+        }
+        else{
         this.salary = salary;
+        }
     }    
 
     public double getBaseSalary() {
@@ -61,7 +67,7 @@ public class BasePlusCommissionEmployeeComposition {
 
     public double getGrossSales() {
     	// implemented this method
-        return sales;
+        return ce.gross;
     }
 
     public void setGrossSales(double sales) {
@@ -71,12 +77,12 @@ public class BasePlusCommissionEmployeeComposition {
 
     public double getCommissionRate() {
     	// implemented this method
-        return rate;
+        return ce.commission;
     }
 
     public void setCommissionRate(double rate) {
     	// implemented this method
-        this.rate = rate;
+        this.commission = commission;
     }    
     
     public double earnings() {
@@ -87,5 +93,8 @@ public class BasePlusCommissionEmployeeComposition {
     @Override
     public String toString() {        
     	// TODO: implement this method
+        return String.format("%s: %s%n%s: %s%n%s: %s%n%s: %f%n%s: %f%n%s: %f%n","First Name", getFirstName(),
+                "Last Name", getLastName(), "SSN", getSocialSecurityNumber(), "Commission", getCommissionRate(),
+                "Gross", getGrossSales(), "Base Salary", getBaseSalary());
     }
 }
