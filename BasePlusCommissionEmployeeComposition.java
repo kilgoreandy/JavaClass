d924b69b46b00220b6e5ef735ee382389c93a134
kilgoreandy <92450298+kilgoreandy@users.noreply.github.com>
2022-05-16 14:40:43 -0500

    added info to the  stubs for the methods

--- BasePlusCommissionEmployeeComposition.java
+++ BasePlusCommissionEmployeeComposition.java
@@ -2,7 +2,20 @@
  * BasePlusCommissionEmployeeComposition.java
  */
 
+ /*
+ 9.3 (Recommended: Using Composition Rather Than Inheritance) Many
+programs written with inheritance could be written with composition
+instead, and vice versa. Rewrite class BasePlus–
+CommissionEmployee (Fig. 9.11 ) of the
+CommissionEmployee–BasePlusCommissionEmployee
+hierarchy so that it contains a reference to a CommissionEmployee
+object, rather than inheriting from class CommissionEmployee. Retest
+BasePlusCommissionEmployee to demonstrate that it still provides
+the same functionality.
+*/ 
+
 public class BasePlusCommissionEmployeeComposition {
+    BasePlusCommissionEmployee bpce = new BasePlusCommissionEmployee();
     
     private double baseSalary; // base salary per week
     // Demonstrate composition of (as opposed to inheritance from) a CommissionEmployee object
@@ -10,49 +23,65 @@ public class BasePlusCommissionEmployeeComposition {
     // six-argument constructor
     public BasePlusCommissionEmployeeComposition( String first, String last, 
             String ssn, double sales, double rate, double salary) {
-    	// TODO: implement this constructor
+    	// implemented the 6 argument constructor
+        this.first = first;
+        this.last = last;
+        this.ssn = ssn;
+        this.sales = sales;
+        this.rate = rate;
+        this.salary = salary; 
     }
     
     // Implement accessors and mutators for all six attributes (stubs appear below)
     // As per the text (demonstrated in its BasePlusCommissionEmployee implementation), throw an IllegalArgumentException if the salary argument is negative
     public void setBaseSalary( double salary ) {
-    	// TODO: implement this method
+    	// implemented this method
+        this.salary = salary;
     }    
 
     public double getBaseSalary() {
-    	// TODO: implement this method
+    	// implemented this method
+        return salary;
     }
 
     public String getFirstName() {
-    	// TODO: implement this method
+    	// implemented this method
+        return first;
     }
 
     public String getLastName() {
-    	// TODO: implement this method
+    	// implemented this method
+        return last;
     }
 
     public String getSocialSecurityNumber() {
-    	// TODO: implement this method
+    	// implemented this method
+        return ssn;
     }
 
     public double getGrossSales() {
-    	// TODO: implement this method
+    	// implemented this method
+        return sales;
     }
 
     public void setGrossSales(double sales) {
-    	// TODO: implement this method
+    	// implemented this method
+        this.sales = sales;
     }
 
     public double getCommissionRate() {
-    	// TODO: implement this method
+    	// implemented this method
+        return rate;
     }
 
     public void setCommissionRate(double rate) {
-    	// TODO: implement this method
+    	// implemented this method
+        this.rate = rate;
     }    
     
     public double earnings() {
     	// TODO: implement this method
+        return double earnings;
     }
     
     @Override
